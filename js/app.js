96a2908efbd7ab675237754b502bac711b45554a
Arvin Alvea <arvinalvea@gmail.com>
2020-10-12 09:23:11 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,15 +13,15 @@ const navSlide = () => {
         nav.classList.toggle("nav-active");
 
         //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = "";
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${
-                    index / 7 + 0.5
-                }s`;
-            }
-        });
+        // navLinks.forEach((link, index) => {
+        //     if (link.style.animation) {
+        //         link.style.animation = "";
+        //     } else {
+        //         link.style.animation = `navLinkFade 0.5s ease forwards ${
+        //             index / 7 + 0.5
+        //         }s`;
+        //     }
+        // });
 
         //Burger Animation
         //burger.classList.toggle("toggle");
