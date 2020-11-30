45789552a5a0d2e5c8b284db42dca8ac07692be3
Arvin Alvea <arvinalvea@gmail.com>
2020-11-30 15:43:28 -0500

    Updated titles + updated contact.php

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 $from = 'info@closeyourmove.ca';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'arvinalvea@gmail.com';
+$sendTo = 'pdaloisio@avagiolaw.com';
 
 // subject of the email
 $subject = 'New message from contact form';
